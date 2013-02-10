8865e6cbeb772f2e19bc4194bdd2ca7795816d71
JaredHatfield <jjhatf02@gmail.com>
2013-02-10 15:28:18 -0500

    First version of sendmailses that works with Amazon Linux
    The initial version of sendmailses is able to configure Amazon Linux to
    have sendmail uses SES to send all email.

---
+++ manifests/init.pp
@@ -0,0 +1,64 @@
+
+class sendmailses (
+  $smtp_user,
+  $smtp_authentication,
+  $smtp_password) {
+
+  package { "sendmail":
+    ensure => latest,
+  }
+
+  package { "sendmail-cf":
+    ensure => latest,
+  }
+
+  package { "m4":
+    ensure => latest,
+  }
+
+  service { "sendmail":
+    ensure  => "running",
+    enable  => "true",
+    require => Package["sendmail"],
+  }
+
+  file { "/etc/mail/authinfo":
+    owner   => root,
+    group   => root,
+    mode    => 444,
+    content => template("sendmailses/authinfo.erb"),
+    require => Package["sendmail"],
+  }
+
+  exec { "updateauthinfo":
+    require     => File["/etc/mail/authinfo"],
+    path        => ["/usr/bin", "/usr/sbin"],
+    command     => "makemap hash /etc/mail/authinfo.db < /etc/mail/authinfo",
+    subscribe   => File["/etc/mail/authinfo"],
+    refreshonly => true,
+    notify      => Service["sendmail"],
+  }
+
+  file { "/etc/mail/sendmail.mc":
+    owner   => root,
+    group   => root,
+    mode    => 644,
+    source  => "puppet:///modules/sendmailses/sendmail.mc",
+    require => Package["sendmail"],
+  }
+
+  file { "/etc/mail/sendmail.cf":
+    owner => root,
+    group => root,
+    mode  => 644,
+  }
+
+  exec { "updatesendmail":
+    require     => [File["/etc/mail/sendmail.cf"], File["/etc/mail/sendmail.mc"], Package["m4"], Package["sendmail-cf"]],
+    path        => ["/usr/bin", "/usr/sbin"],
+    command     => "m4 /etc/mail/sendmail.mc > /etc/mail/sendmail.cf",
+    subscribe   => File["/etc/mail/sendmail.mc"],
+    refreshonly => true,
+    notify      => Service["sendmail"],
+  }
+}
