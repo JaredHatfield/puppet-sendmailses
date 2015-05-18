163125c9dd703765217e2981271dc1af7e1a16f4
Jared Hatfield <jjhatf02@gmail.com>
2015-05-17 20:19:04 -0400

    Fixed puppet lint errors

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,64 +1,71 @@
-
+# = Class: sendmailses
+#
+# Manages Sendmail for SES.
+#
+#
 class sendmailses (
   $smtp_user,
   $smtp_authentication,
   $smtp_password) {
 
-  package { "sendmail":
+  package { 'sendmail':
     ensure => latest,
   }
 
-  package { "sendmail-cf":
+  package { 'sendmail-cf':
     ensure => latest,
   }
 
-  package { "m4":
+  package { 'm4':
     ensure => latest,
   }
 
-  service { "sendmail":
-    ensure  => "running",
-    enable  => "true",
-    require => Package["sendmail"],
+  service { 'sendmail':
+    ensure  => 'running',
+    enable  => true,
+    require => Package['sendmail'],
   }
 
-  file { "/etc/mail/authinfo":
+  file { '/etc/mail/authinfo':
     owner   => root,
     group   => root,
-    mode    => 444,
-    content => template("sendmailses/authinfo.erb"),
-    require => Package["sendmail"],
+    mode    => '0444',
+    content => template('sendmailses/authinfo.erb'),
+    require => Package['sendmail'],
   }
 
-  exec { "updateauthinfo":
-    require     => File["/etc/mail/authinfo"],
-    path        => ["/usr/bin", "/usr/sbin"],
-    command     => "makemap hash /etc/mail/authinfo.db < /etc/mail/authinfo",
-    subscribe   => File["/etc/mail/authinfo"],
+  exec { 'updateauthinfo':
+    require     => File['/etc/mail/authinfo'],
+    path        => ['/usr/bin', '/usr/sbin'],
+    command     => 'makemap hash /etc/mail/authinfo.db < /etc/mail/authinfo',
+    subscribe   => File['/etc/mail/authinfo'],
     refreshonly => true,
-    notify      => Service["sendmail"],
+    notify      => Service['sendmail'],
   }
 
-  file { "/etc/mail/sendmail.mc":
+  file { '/etc/mail/sendmail.mc':
     owner   => root,
     group   => root,
-    mode    => 644,
-    source  => "puppet:///modules/sendmailses/sendmail.mc",
-    require => Package["sendmail"],
+    mode    => '0644',
+    source  => 'puppet:///modules/sendmailses/sendmail.mc',
+    require => Package['sendmail'],
   }
 
-  file { "/etc/mail/sendmail.cf":
+  file { '/etc/mail/sendmail.cf':
     owner => root,
     group => root,
-    mode  => 644,
+    mode  => '0644',
   }
 
-  exec { "updatesendmail":
-    require     => [File["/etc/mail/sendmail.cf"], File["/etc/mail/sendmail.mc"], Package["m4"], Package["sendmail-cf"]],
-    path        => ["/usr/bin", "/usr/sbin"],
-    command     => "m4 /etc/mail/sendmail.mc > /etc/mail/sendmail.cf",
-    subscribe   => File["/etc/mail/sendmail.mc"],
+  exec { 'updatesendmail':
+    require     => [File['/etc/mail/sendmail.cf'],
+                    File['/etc/mail/sendmail.mc'],
+                    Package['m4'],
+                    Package['sendmail-cf']],
+    path        => ['/usr/bin', '/usr/sbin'],
+    command     => 'm4 /etc/mail/sendmail.mc > /etc/mail/sendmail.cf',
+    subscribe   => File['/etc/mail/sendmail.mc'],
     refreshonly => true,
-    notify      => Service["sendmail"],
+    notify      => Service['sendmail'],
   }
 }
